705a1ed0aec382669167d53616a900be4c938a3e
Jonathan Bijos <jonathanpbijos@gmail.com>
2020-04-14 10:02:37 -0300

    fix decimal format

--- Covid19Tracker/Extensions/Double+DecimalFormat.swift
+++ Covid19Tracker/Extensions/Double+DecimalFormat.swift
@@ -16,37 +16,22 @@ extension Double {
         return formatter.string(from: NSNumber(value: self))
     }
 
-    func truncate(places: Int) -> Double {
-
-        let multiplier = pow(10, Double(places))
-        let newDecimal = multiplier * self // move the decimal right
-        let truncated = Double(Int(newDecimal)) // drop the fraction
-        let originalDecimal = truncated / multiplier // move the decimal back
-        return originalDecimal
-
-    }
-
     var shortened: String {
-        let num = abs(self)
-        let sign = (self < 0) ? "-" : ""
-
-        switch num {
+        switch self {
+        case 1_000_000_000_000...:
+            guard let formatted = (self / 1_000_000_000_000).decimalFormat else { return "--" }
+            return "\(formatted)T"
         case 1_000_000_000...:
-            var formatted = num / 1_000_000_000
-            formatted = formatted.truncate(places: 1)
-            return "\(sign)\(formatted)B"
+            guard let formatted = (self / 1_000_000_000).decimalFormat else { return "--" }
+            return "\(formatted)B"
         case 1_000_000...:
-            var formatted = num / 1_000_000
-            formatted = formatted.truncate(places: 1)
-            return "\(sign)\(formatted)M"
+            guard let formatted = (self / 1_000_000).decimalFormat else { return "--" }
+            return "\(formatted)M"
         case 1_000...:
-            var formatted = num / 1_000
-            formatted = formatted.truncate(places: 1)
-            return "\(sign)\(formatted)K"
-        case 0...:
-            return "\(self)"
+            guard let formatted = (self / 1_000).decimalFormat else { return "--" }
+            return "\(formatted)K"
         default:
-            return "\(sign)\(self)"
+            return "\(Int(self))"
         }
     }
 }
