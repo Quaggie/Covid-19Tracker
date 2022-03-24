972e2179da2a4912c8666b87f2f61ff43b633487
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-03-24 19:04:19 -0300

    Add podfile configurations

---
+++ Pods/Target Support Files/Pods-Covid19Tracker-Covid19Tracker_unit_tests/Pods-Covid19Tracker-Covid19Tracker_unit_tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covid19Tracker_Covid19Tracker_unit_tests : NSObject
+@end
+@implementation PodsDummy_Pods_Covid19Tracker_Covid19Tracker_unit_tests
+@end
