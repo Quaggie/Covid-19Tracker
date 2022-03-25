242c6f1cb5117c8d34cc506b445f3aa384351ab8
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-03-24 21:09:40 -0300

    Add tabviewcontroller delegate implementation test

--- Covid19Tracker Unit Tests/Components/TabBar/TabBarViewControllerTests.swift
+++ Covid19Tracker Unit Tests/Components/TabBar/TabBarViewControllerTests.swift
@@ -11,7 +11,24 @@ import XCTest
 
 class TabBarViewControllerTests: XCTestCase {
     func test_init_configureViewControllers() {
-        let sut = TabBarViewController(nibName: nil, bundle: nil)
+        let sut = TabBarViewController()
+
         XCTAssertEqual(sut.viewControllers?.count, 5)
+        XCTAssertTrue(sut.tabBar.isOpaque)
+        XCTAssertEqual(sut.tabBar.backgroundColor, Color.white)
+        XCTAssertNotNil(sut.delegate)
+    }
+
+    func test_tabViewControllerShouldViewController_shouldSelectOnlyIfMiddleViewControllerIsGiven() {
+        let sut = TabBarViewController()
+
+        let shouldSelect = { (viewController: UIViewController) -> Bool in
+            return sut.tabBarController(sut, shouldSelect: viewController)
+        }
+        XCTAssertTrue(shouldSelect(sut.viewControllers![0]))
+        XCTAssertTrue(shouldSelect(sut.viewControllers![1]))
+        XCTAssertFalse(shouldSelect(sut.viewControllers![2]))
+        XCTAssertTrue(shouldSelect(sut.viewControllers![3]))
+        XCTAssertTrue(shouldSelect(sut.viewControllers![4]))
     }
 }
