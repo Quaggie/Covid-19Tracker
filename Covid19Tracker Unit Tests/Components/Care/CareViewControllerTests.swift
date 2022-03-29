f9f0f67b8d135812f3286eff7ab5d9b910185b2a
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-03-29 18:47:16 -0300

    Guarantee preferredStatusBarStyle is lightcontent

--- Covid19Tracker Unit Tests/Components/Care/CareViewControllerTests.swift
+++ Covid19Tracker Unit Tests/Components/Care/CareViewControllerTests.swift
@@ -9,6 +9,16 @@
 import XCTest
 @testable import Covid19_Tracker
 
-class CareViewControllerTests: XCTestCase {
+class CareViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
 
+class CareViewControllerTests: XCTestCase {
+    func test_preferredStatusBarStyle_isCorrectType() {
+        let sut = CareViewController()
+
+        XCTAssertEqual(sut.preferredStatusBarStyle, .lightContent)
+    }
 }
