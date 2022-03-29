1150e1d8f87216e159df480c296e2e8fefa6e240
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-03-29 18:41:43 -0300

    Create CareViewController test file

---
+++ Covid19Tracker Unit Tests/Components/Care/CareViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  CareViewControllerTests.swift
+//  Covid19Tracker_unit_tests
+//
+//  Created by Jonathan Bijos on 29/03/22.
+//  Copyright © 2022 DevsCarioca. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19_Tracker
+
+class CareViewControllerTests: XCTestCase {
+
+}
