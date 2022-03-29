e71e681a69b11e1166df06ee94f6b806e945a8a2
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-03-29 19:01:52 -0300

    Test viewDidAppear and guarantee tracker is hitting the screenviews

--- Covid19Tracker Unit Tests/Components/Care/CareViewControllerTests.swift
+++ Covid19Tracker Unit Tests/Components/Care/CareViewControllerTests.swift
@@ -9,24 +9,58 @@
 import XCTest
 @testable import Covid19_Tracker
 
-class CareViewController: BaseViewController {
+class CareViewController: UIViewController {
+    let tracker: TrackerProtocol
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+
+    init(tracker: TrackerProtocol) {
+        self.tracker = tracker
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        tracker.screenView(name: String(describing: self))
     }
 }
 
 class CareViewControllerTests: XCTestCase {
     func test_preferredStatusBarStyle_isCorrectType() {
-        let sut = makeSUT()
+        let (sut, _) = makeSUT()
 
         XCTAssertEqual(sut.preferredStatusBarStyle, .lightContent)
+    }
+
+    func test_viewDidAppear_tracksScreenView() {
+        let (sut, tracker) = makeSUT()
+
+        sut.viewDidAppear(false)
+
+        XCTAssertEqual(tracker.screenViews[0], String(describing: sut))
     }
 }
 
 extension CareViewControllerTests {
-    func makeSUT() -> CareViewController {
-        let viewController = CareViewController()
+    func makeSUT() -> (CareViewController, TrackerSpy) {
+        let tracker = TrackerSpy()
+        let viewController = CareViewController(tracker: tracker)
         checkMemoryLeak(for: viewController)
-        return viewController
+        checkMemoryLeak(for: tracker)
+        return (viewController, tracker)
+    }
+}
+
+final class TrackerSpy: TrackerProtocol {
+    var screenViews: [String] = []
+
+    func screenView(name: String) {
+        screenViews.append(name)
     }
 }
