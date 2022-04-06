5956aa38f03c7b99b76975e6214450ff2321ebfe
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-04-06 20:13:09 -0300

    Guarantee parameters with and without spaces work with CountryService Extract duplicated test logic into helper method

--- DataTests/Services/CountryServiceTests.swift
+++ DataTests/Services/CountryServiceTests.swift
@@ -14,45 +14,46 @@ final class CountryServiceTests: XCTestCase {
     func test_fetchCountry_transformsCorrectURLParamsWhenPassingStringWithoutSpaces() {
         let (sut, networkManager) = makeSUT()
         let country = "Brazil"
-
-        let exp = expectation(description: #function)
-        sut.fetch(country: country) { result in
-            exp.fulfill()
-        }
-        let countryModel = CountryModel(country: "", countryInfo: .init(flag: ""), cases: 0, todayCases: 0, deaths: 0, todayDeaths: 0, recovered: 0, active: 0)
-        let data = try! JSONEncoder().encode(countryModel)
-        networkManager.complete(with: .success(data))
-        wait(for: [exp], timeout: 1)
-
         let expectedParameter = "/countries/\(country)"
-        XCTAssertEqual(networkManager.urlStrings, [expectedParameter])
+
+        expect(sut: sut, country: country, networkManager: networkManager, with: expectedParameter) {
+            let countryModel = CountryModel(country: "", countryInfo: .init(flag: ""), cases: 0, todayCases: 0, deaths: 0, todayDeaths: 0, recovered: 0, active: 0)
+            let data = try! JSONEncoder().encode(countryModel)
+            networkManager.complete(with: .success(data))
+        }
+    }
+
+    func test_fetchCountry_transformsCorrectURLParamsWhenPassingStringWithSpaces() {
+        let (sut, networkManager) = makeSUT()
+        let country = "New Zealand"
+        let expectedParameter = "/countries/\(country.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)"
+
+        expect(sut: sut, country: country, networkManager: networkManager, with: expectedParameter) {
+            let countryModel = CountryModel(country: "", countryInfo: .init(flag: ""), cases: 0, todayCases: 0, deaths: 0, todayDeaths: 0, recovered: 0, active: 0)
+            let data = try! JSONEncoder().encode(countryModel)
+            networkManager.complete(with: .success(data))
+        }
     }
 
     func test_fetchCountry_returnsCorrectCountryWhenPassingStringWithoutSpaces() {
         let (sut, networkManager) = makeSUT()
-        let country = "New Zealand"
-
-        let exp = expectation(description: #function)
-        var receivedResult: Result<CountryModel, WebserviceError>?
-        sut.fetch(country: country) { result in
-            receivedResult = result
-            exp.fulfill()
-        }
+        let country = "Brazil"
 
         let countryModel = CountryModel(country: "", countryInfo: .init(flag: ""), cases: 0, todayCases: 0, deaths: 0, todayDeaths: 0, recovered: 0, active: 0)
-        let data = try! JSONEncoder().encode(countryModel)
-        networkManager.complete(with: .success(data))
+        expect(sut: sut, country: country, with: .success(countryModel)) {
+            let data = try! JSONEncoder().encode(countryModel)
+            networkManager.complete(with: .success(data))
+        }
+    }
 
-        wait(for: [exp], timeout: 1)
-        let expectedResult: Result<CountryModel, WebserviceError> = .success(countryModel)
+    func test_fetchCountry_returnsCorrectCountryWhenPassingStringWithSpaces() {
+        let (sut, networkManager) = makeSUT()
+        let country = "New Zealand"
 
-        switch (receivedResult, expectedResult) {
-        case (.success(let receivedData), .success(let expectedData)):
-            XCTAssertEqual(receivedData, expectedData)
-        case (.failure(let receivedError), .failure(let expectedError)):
-            XCTAssertEqual(receivedError, expectedError)
-        default:
-            XCTFail("Should've completed with \(expectedResult) instead got \(String(describing: receivedResult))")
+        let countryModel = CountryModel(country: "", countryInfo: .init(flag: ""), cases: 0, todayCases: 0, deaths: 0, todayDeaths: 0, recovered: 0, active: 0)
+        expect(sut: sut, country: country, with: .success(countryModel)) {
+            let data = try! JSONEncoder().encode(countryModel)
+            networkManager.complete(with: .success(data))
         }
     }
 }
@@ -64,5 +65,36 @@ extension CountryServiceTests {
         checkMemoryLeak(for: networkManager, file: file, line: line)
         checkMemoryLeak(for: service, file: file, line: line)
         return (service, networkManager)
+    }
+
+    func expect(sut: CountryService, country: String, with expectedResult: Result<CountryModel, WebserviceError>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: #function)
+        var receivedResult: Result<CountryModel, WebserviceError>?
+        sut.fetch(country: country) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        action()
+        wait(for: [exp], timeout: 1)
+
+        switch (receivedResult, expectedResult) {
+        case (.success(let receivedData), .success(let expectedData)):
+            XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+        case (.failure(let receivedError), .failure(let expectedError)):
+            XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+        default:
+            XCTFail("Should've completed with \(expectedResult) instead got \(String(describing: receivedResult))", file: file, line: line)
+        }
+    }
+
+    func expect(sut: CountryService, country: String, networkManager: NetworkManagerSpy, with expectedParameter: String, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: #function)
+        sut.fetch(country: country) { _ in
+            exp.fulfill()
+        }
+        action()
+        wait(for: [exp], timeout: 1)
+
+        XCTAssertEqual(networkManager.urlStrings, [expectedParameter])
     }
 }
