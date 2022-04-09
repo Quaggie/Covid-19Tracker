c47c1696525c0465c5f8c61d2421519b041aba51
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-04-09 15:10:46 -0300

    Add new Domain framework to hold Usecase protocols Add WorldOverviewService to implement WorldOverviewUseCase

---
+++ Data/UseCases/WorldOverviewService.swift
@@ -0,0 +1,16 @@
+//
+//  WorldOverviewService.swift
+//  Data
+//
+//  Created by Jonathan Bijos on 09/04/22.
+//  Copyright © 2022 DevsCarioca. All rights reserved.
+//
+
+import Foundation
+import Domain
+
+final class WorldOverviewService: WorldOverviewUseCase {
+    func fetch() {
+
+    }
+}
---
+++ Domain/WorldOverviewUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  WorldOverviewUseCase.swift
+//  Domain
+//
+//  Created by Jonathan Bijos on 09/04/22.
+//  Copyright © 2022 DevsCarioca. All rights reserved.
+//
+
+public protocol WorldOverviewUseCase {
+    func fetch()
+}
