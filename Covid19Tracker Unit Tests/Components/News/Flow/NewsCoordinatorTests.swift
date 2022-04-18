108b0781b9007d88674a4d26565405026fa74e5f
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-04-18 10:09:25 -0300

    Create empty NewsCoordinatorTests and organize folders

---
+++ Covid19Tracker Unit Tests/Components/News/Flow/NewsCoordinatorTests.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCoordinatorTests.swift
+//  Covid19Tracker_unit_tests
+//
+//  Created by Jonathan Bijos on 18/04/22.
+//  Copyright © 2022 DevsCarioca. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19_Tracker
+
+class NewsCoordinatorTests: XCTestCase {
+    
+}
