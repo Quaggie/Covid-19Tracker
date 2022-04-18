f4636a5e0ef397a609c17176eee4643ddb4a3a84
Jonathan Bijos <jonathan.bijos@pebmed.com.br>
2022-04-18 15:01:40 -0300

    Add care coordinator tests Organize care folder structure for tests Add helper for news coordinator tests

--- Covid19Tracker Unit Tests/Components/Care/Flow/CareCoordinatorTests.swift
+++ Covid19Tracker Unit Tests/Components/Care/Flow/CareCoordinatorTests.swift
@@ -1,6 +1,6 @@
 //
-//  NewsCoordinatorTests.swift
-//  Covid19Tracker_unit_tests
+//  CareCoordinatorTests.swift
+//  Covid19Tracker
 //
 //  Created by Jonathan Bijos on 18/04/22.
 //  Copyright © 2022 DevsCarioca. All rights reserved.
@@ -9,19 +9,19 @@
 import XCTest
 @testable import Covid19_Tracker
 
-class NewsCoordinatorTests: XCTestCase {
-    func test_rootViewController_isNewsViewController() {
+class CareCoordinatorTests: XCTestCase {
+    func test_rootViewController_isCareViewController() {
         let (_, viewControllerPresenter) = makeSUT()
 
         XCTAssertEqual(viewControllerPresenter.viewControllers.count, 1)
-        XCTAssert(viewControllerPresenter.viewControllers[0] is NewsViewController)
+        XCTAssert(viewControllerPresenter.viewControllers[0] is CareViewController)
     }
 }
 
-extension NewsCoordinatorTests {
-    func makeSUT() -> (NewsCoordinator, ViewControllerPresenterSpy) {
+extension CareCoordinatorTests {
+    func makeSUT() -> (CareCoordinator, ViewControllerPresenterSpy) {
         let viewControllerPresenter = ViewControllerPresenterSpy()
-        let coordinator = NewsCoordinator(parent: viewControllerPresenter)
+        let coordinator = CareCoordinator(parent: viewControllerPresenter)
         checkMemoryLeak(for: coordinator)
         checkMemoryLeak(for: viewControllerPresenter)
         coordinator.start()
